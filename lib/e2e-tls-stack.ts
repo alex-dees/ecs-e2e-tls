870db5df31be71e560aef7be2797771ba3dcf3db
Alex Dees <alex-dees@github.com>
2023-03-21 13:36:50 -0500

    wip

--- lib/e2e-tls-stack.ts
+++ lib/e2e-tls-stack.ts
@@ -3,9 +3,11 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
 import * as patterns from 'aws-cdk-lib/aws-ecs-patterns';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { DockerImageAsset } from 'aws-cdk-lib/aws-ecr-assets';
+import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 
 export class E2ETlsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -14,19 +16,35 @@ export class E2ETlsStack extends cdk.Stack {
     //const vpc = new ec2.Vpc(this, 'Vpc');
 
     const app = new DockerImageAsset(this, 'App', {
-      directory: path.join(__dirname, '../src/app')
+      // directory: path.join(__dirname, '../src/app')
+      directory: 'src/app'
     });
+
     const proxy = new DockerImageAsset(this, 'Proxy', {
       directory: 'src/proxy'
     });
-    /*
-    const service = new patterns
-      .ApplicationLoadBalancedFargateService(
-        this, 
-        'Service', {
-          // vpc,
-          // targetProtocol: elb.ApplicationProtocol.HTTPS
-        });
-    */
+
+    const cert = Certificate.fromCertificateArn(this, 'Cert', 
+      'arn:aws:acm:us-east-1:844540003076:certificate/4fe693fd-6195-438d-acf5-7d3f59198871');
+
+    const service = new patterns.ApplicationLoadBalancedFargateService(this, 'Service', {
+      certificate: cert,
+      targetProtocol: elb.ApplicationProtocol.HTTPS,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromDockerImageAsset(app)
+      }
+    });
+    service.targetGroup.configureHealthCheck({
+      path: '/service'
+    })
+    
+    const sideCar = service.taskDefinition.addContainer('Sidecar', {
+      image: ecs.ContainerImage.fromDockerImageAsset(proxy)
+    });
+
+    sideCar.addPortMappings({
+      hostPort: 443,
+      containerPort: 443
+    })
   }
 }
